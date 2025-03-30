1d15af663f93f7baadc16898d5becfcbefa5f1eb
realone214 <115413275+realone214@users.noreply.github.com>
2025-03-30 11:46:22 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,76 @@
+
+const express = require('express');
+const app = express();
+const http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io');
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, { 
+  cors: { 
+    origin: "http://192.168.254.107:3000", 
+    methods: ["GET", "POST"],
+  },
+});
+
+const rooms = {};
+const activeUsers = {};
+const allMessages = {};
+
+io.on('connection', (socket) => {
+  socket.on('join_room', ({ room, userName }) => {
+    socket.join(room);
+    socket.userName = userName;
+
+    if (!rooms[room]) {
+      rooms[room] = [];
+    }
+
+    if (!activeUsers[room]) {
+      activeUsers[room] = new Set();
+    }
+
+    activeUsers[room].add(userName);
+    io.to(room).emit('active_users', Array.from(activeUsers[room]));
+
+    rooms[room].push(socket.id);
+    socket.emit('joined_room', room);
+    socket.to(room).emit('update_user_count', { room, userCount: rooms[room].length });
+
+    if (allMessages[room]) {
+      socket.emit('all_messages', allMessages[room]);
+    }
+  });
+
+  socket.on('send_message', (data) => {
+    if (!allMessages[data.room]) {
+      allMessages[data.room] = [];
+    }
+    allMessages[data.room].push(data);
+    socket.to(data.room).emit('receive_message', data);
+    socket.emit('receive_message', data); // Echo back to the sender
+  });
+
+  socket.on('leave_room', ({ room, userName }) => {
+    if (activeUsers[room]) {
+      activeUsers[room].delete(userName);
+      io.to(room).emit('active_users', Array.from(activeUsers[room]));
+    }
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+    for (const room in activeUsers) {
+      if (activeUsers[room].has(socket.userName)) {
+        activeUsers[room].delete(socket.userName);
+        io.to(room).emit('active_users', Array.from(activeUsers[room]));
+      }
+    }
+  });
+});
+
+server.listen(3001, () => {
+  console.log('listening on *:3001');
+});
